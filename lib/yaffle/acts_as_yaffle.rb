33932930504adf8f76187c5942e3886d64156135
alectrico <ventas@alectrico.cl>
2025-06-15 19:27:41 -0400

    Pasó las pruebas para los métodos de instancia

--- lib/yaffle/acts_as_yaffle.rb
+++ lib/yaffle/acts_as_yaffle.rb
@@ -6,5 +6,10 @@ module Yaffle
         cattr_accessor :yaffle_text_field, default: (options[:yaffle_text_field] || :last_squawk).to_s
       end
     end
+    included do
+      def squawk( string )
+        write_attribute(self.class.yaffle_text_field, string.to_squawk)
+      end
+    end
   end
 end
